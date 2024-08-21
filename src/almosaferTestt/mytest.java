8306cdc9ce7be6aabd71351f332191f32b9f0977
mothafer <mothafer77@gmail.com>
2024-08-21 20:23:59 +0300

    update

--- src/almosaferTestt/mytest.java
+++ src/almosaferTestt/mytest.java
@@ -6,9 +6,11 @@ import java.util.List;
 import java.util.Random;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.By.ByCssSelector;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -23,7 +25,7 @@ public class mytest {
 	public void Mysetup() {
 		driver.get(mywebsite);
 		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 		
 			
 	}
@@ -106,4 +108,86 @@ public class mytest {
 		
 		
 	}
-}
+	
+	@Test(priority = 8)
+	public void FillHotelTap () {
+		
+		String[] Englishcity = {"Dubbai","jeddah","riyadh"};
+		int randomENcity = rand.nextInt(Englishcity.length);
+		
+		String[] Arabiccity = {"دبي","جده"};
+		int randomARcity = rand.nextInt(Arabiccity.length);
+
+		
+		
+		
+		WebElement hotelTap = driver.findElement(By.id("uncontrolled-tab-example-tab-hotels"));
+		hotelTap.click();
+		
+		WebElement searchHotel = driver.findElement(By.xpath("//input[@data-testid='AutoCompleteInput']"));
+		
+		String webSiteURLs = driver.getCurrentUrl();
+		
+		if(webSiteURLs.contains("ar")) {
+			
+			searchHotel.sendKeys(Englishcity[randomENcity]);
+		}else {
+			searchHotel.sendKeys(Arabiccity[randomARcity]);
+		}
+		WebElement ListtofLocation = driver.findElement(By.cssSelector(".sc-phbroq-4.gGwzVo.AutoComplete__List"));
+		WebElement firstResult = ListtofLocation.findElements(By.tagName("li")).get(2);
+		firstResult.click();
+	}
+	
+	@Test(priority = 9)
+	public void RandomlyselectTheNumberOfvistor() {
+		WebElement selectoro = driver.findElement(By.xpath("//select[@data-testid='HotelSearchBox__ReservationSelect_Select']"));
+		Select select = new Select(selectoro);
+		 int randomindx = rand.nextInt(2);
+		 select.selectByIndex(randomindx);
+		 
+		 WebElement buttonsearch = driver.findElement(By.xpath("//button[@data-testid='HotelSearchBox__SearchButton']"));
+		 buttonsearch.click();
+		
+		
+		
+	}
+	
+	@Test(priority = 10)
+	public void CheckThePageloaded() throws InterruptedException {
+		Thread.sleep(10000);
+		String result = driver.findElement(By.xpath("//span[@data-testid='HotelSearchResult__resultsFoundCount']")).getText();
+	boolean actual	= result.contains("found")||result.contains("وجدنا ");
+	boolean expected = true;
+	Assert.assertEquals(actual, expected);
+		
+		
+	}
+	
+	@Test(priority = 11)
+	public void SortItem() {
+		WebElement LowestpriceButton= driver.findElement(By.xpath("//button[@data-testid='HotelSearchResult__sort__LOWEST_PRICE']"));
+		LowestpriceButton.click();
+		
+		WebElement pricesContainer = driver.findElement(By.cssSelector(".sc-htpNat.KtFsv.col-9"));
+		List<WebElement> allprices = pricesContainer.findElements(By.className("Price__Value"));
+		
+		String lowestprices = allprices.get(0).getText();
+		String Highestprices = allprices.get(allprices.size()-1).getText();
+		
+		
+		int lowestpricesINT = Integer.parseInt(lowestprices);
+		int HighestpricesINT =Integer.parseInt(Highestprices);
+		
+		boolean actual = lowestpricesINT < HighestpricesINT;
+		 boolean expected =true;
+		 
+		 Assert.assertEquals(actual, expected);
+		 
+		
+		
+		
+	}
+		
+	}
+
